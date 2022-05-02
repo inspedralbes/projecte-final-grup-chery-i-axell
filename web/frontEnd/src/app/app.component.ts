7090b29cff58683f64f70997470879c2bbfd1dd3
a19axechacan <a19axechacan@inspedralbes.cat>
2022-05-02 08:52:46 +0200

    progres configuracio firebase

--- web/frontEnd/src/app/app.component.ts
+++ web/frontEnd/src/app/app.component.ts
@@ -1,10 +1,22 @@
 import { Component } from '@angular/core';
-
+import { Angul } from '@angular/fire/database';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+
   title = 'frontEnd';
+
+
+
+  constructor(private db: AngularFireDatabase) {
+
+
+
+  }
+
+
+
 }
--- web/frontEnd/src/environments/environment.ts
+++ web/frontEnd/src/environments/environment.ts
@@ -3,6 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
+  firebase: {
+    projectId: 'projecte-daw',
+    appId: '1:470236089665:web:008aa55fadefa85c47e5c0',
+    databaseURL: 'https://projecte-daw-default-rtdb.europe-west1.firebasedatabase.app',
+    storageBucket: 'projecte-daw.appspot.com',
+    apiKey: 'AIzaSyA2z_4l7_OhSMPnX0soQwD3QHo3Z3xJo9U',
+    authDomain: 'projecte-daw.firebaseapp.com',
+    messagingSenderId: '470236089665',
+    measurementId: 'G-3BQKG35WZQ',
+  },
   production: false
 };
 
