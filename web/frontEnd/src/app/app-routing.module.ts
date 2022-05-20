2a4c0ad13427099fabede3a0b07cd6627bc69919
a19axechacan <92580180+a19axechacan@users.noreply.github.com>
2022-05-20 10:48:58 +0200

    rutes protegides

--- web/frontEnd/src/app/app-routing.module.ts
+++ web/frontEnd/src/app/app-routing.module.ts
@@ -17,12 +17,12 @@ import { AuthGuard } from './shared/auth.guard';
 const routes: Routes = [ 
   {path: 'crearTaula', component: CrearTaulaComponent },
   {path: 'taula/:id', component: TaulaComponent },
-  {path: 'afegirPlat', component: AfegirPlatsComponent},
-  {path: 'adminPlats', component: AdminPlatsComponent},
+  {path: 'afegirPlat', component: AfegirPlatsComponent, canActivate:[AuthGuard]},
+  {path: 'adminPlats', component: AdminPlatsComponent, canActivate:[AuthGuard]},
   {path: 'estatTaula/:id', component: EstatTaulaComponent},
   {path:'' ,component:HomepageComponent},
   {path: 'login', component: LoginComponent},
-  {path: 'cambrers/:idcambrer', component: CambrerComponentComponent},
+  {path: 'cambrers/:idcambrer', component: CambrerComponentComponent , canActivate:[AuthGuard]} ,
   {path: 'adminTaula', component: AdminTaulaComponent, canActivate:[AuthGuard]},
 ];
 
--- web/frontEnd/src/app/shared/auth.guard.ts
+++ web/frontEnd/src/app/shared/auth.guard.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree } from '@angular/router';
+import Swal from 'sweetalert2';
 import { AuthServiceService } from '../services/auth-service.service';
 
 import { EmpleadosService } from '../services/empleados.service';
@@ -20,9 +21,14 @@ export class AuthGuard implements CanActivate {
     route: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): any {
     
-      console
 
      if(this.authService.isLoggedIn()!==true){
+      Swal.fire({
+        title: 'Error!',
+        text: 'No tens accès a aquesta pàgina',
+        icon: 'error',
+        confirmButtonText: 'Ok' 
+      })
       this.router.navigate(['/login'])
      }
      return true
